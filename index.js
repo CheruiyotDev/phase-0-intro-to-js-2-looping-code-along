b7386eaf3ef530b5b26ccd81a846ca6bbd1cbadc
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-01 15:26:40 +0300

    Loopin code along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let names = ["Charlie", "Samip", "Ali"]
+let events = []
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message =`Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(message)
+  }
+  return messages;
+}
+ function countDown(number) {
+  //Using for loop
+  // for (let i = number; i >= 0; i--) {
+  //   console.log(i)
+  // } 
+
+  //Using While Loop
+  while (number >= 0) {
+    console.log(number--);
+  }
+ }
