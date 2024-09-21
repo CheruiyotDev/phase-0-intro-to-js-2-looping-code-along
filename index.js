67aae3f955d174988216c416b0918256a7d82c3a
Cheruiyot Evans <152331758+CheruiyotDev@users.noreply.github.com.>
2024-09-21 12:51:20 +0300

    Add looping functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+  const messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages
+}
+
+function countDown(num) {
+  for (let i = 0; i <= num; i++) {
+    console.log(num - i)
+  }
+}
